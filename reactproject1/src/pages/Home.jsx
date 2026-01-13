1519e9fdf4509bf5dca5c3126e26fe4262b4ae16
athmika2904 <athmika.cs24@bmsce.ac.in>
2026-01-13 21:56:40 +0530

    Homepage  search added

--- reactproject1/src/pages/Home.jsx
+++ reactproject1/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import Moviecard from "../components/Moviecard";
+import { useState } from "react";
 function Home(){
-    const movies=[{id:2,title:"John Wick",releasedate:"2020"}];
+    const[search,setSearch]=useState("");
+    const movies=[{id:1,title:"John Wick",releasedate:"2020"},{id:2,title:"Bohn Wick",releasedate:"2020"}];
+    const handleSearch=()=>{
+
+    }
     return(
         <div className="home">
+            <form onSubmit={handleSearch} className="searchform">
+                <input type="text" placeholder="Search for a movie" className="searchinput" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+                <button type="submit" className="submitbtn">Search</button>
+            </form>
             <div className="moviegrid">
                 {
                     movies.map((m)=>(
-                        <Moviecard movie={m} key={m.id}/>
+                        m.title.toLowerCase().startsWith(search)
+                     &&
+                        (<Moviecard movie={m} key={m.id}/>)
                     ))
                 }
             </div>
