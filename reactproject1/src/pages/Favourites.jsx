c0d2aa196fa13feff392f7b71069f112804ee22f
athmika2904 <athmika.cs24@bmsce.ac.in>
2026-01-14 17:10:36 +0530

    Favourite page added

---
+++ reactproject1/src/pages/Favourites.jsx
@@ -0,0 +1,9 @@
+function Favourites(){
+    return(  
+        <div className="emptyfavs">
+            <h3>No favourite movies added</h3>
+            <p>Start adding movies to your favourites</p>
+        </div>
+    )
+}
+export default Favourites;
